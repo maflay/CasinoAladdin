4f489b2ae24c445960b6c4665c4b39c99bb39e69
juanfelipe <maflay26@gmail.com>
2025-09-27 09:12:14 -0500

    cambio de imagen y consoles

--- components/home/home.js
+++ components/home/home.js
@@ -64,7 +64,7 @@ function toPromociones() {
   fetch(IG_FETCH_URL)
     .then((res) => res.json())
     .then((data) => {
-      console.log("Respuesta Apps Script:", data);
+      // console.log("Respuesta Apps Script:", data);
 
       // si data ya es un array [{url:"..."}, ...]
       const posts = data
@@ -72,7 +72,7 @@ function toPromociones() {
         .slice(0 ,4) // 👈 copia para no mutar el array original
         .map((item) => normalizeIgUrl(item.url));
 
-      console.log("Posts normalizados:", posts);
+      // console.log("Posts normalizados:", posts);
 
       window.IG_POSTS = posts;
       renderInstagramEmbeds("ig-feed", window.IG_POSTS);
