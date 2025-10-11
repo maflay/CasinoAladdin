5662f19ab92313dde2dcca8939d6a2fc53c6f1ca
juanfelipe <maflay26@gmail.com>
2025-10-11 09:26:47 -0500

    cambio de fecha footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -11,3 +11,14 @@ function footerRegister() {
   navegarA(`contacto?email=${email}`);
   document.getElementById("email-footer").value = "";
 }
+
+function dateYear() {
+  const fechaCompleta = new Date().toLocaleString("es-CO", {
+    timeZone: "America/Bogota",
+    year: "numeric",
+  });
+
+  document.getElementById("fecha_site").textContent = fechaCompleta;
+}
+
+dateYear();
