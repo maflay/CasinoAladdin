88ddaa56f4d87248723a4728907d0a7e259c79ce
juanfelipe <maflay26@gmail.com>
2026-01-10 10:39:26 -0500

    cambio menu_anko

--- components/modal/menu/menu_anko.js
+++ components/modal/menu/menu_anko.js
@@ -1,3 +1,6 @@
 //    window.open(
 //         "/document/CARTA_ANKO_VIP_BOGOTA_COMIDAS_MP_ENERO_VERSION_DIGITAL.pdf"
 //       );
+
+window.location.href =
+  "/document/CARTA_ANKO_VIP_BOGOTA_COMIDAS_MP_ENERO_VERSION_DIGITAL.pdf";
