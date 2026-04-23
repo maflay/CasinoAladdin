83f66fc064d1651137bcc87ae540de9d5a74d9d5
juanfelipe <maflay26@gmail.com>
2026-04-23 16:04:50 -0500

    update promo

--- components/promociones/promocion-view/promocion-view.js
+++ components/promociones/promocion-view/promocion-view.js
@@ -73,10 +73,10 @@
 
     if (linkHash === currentHash) {
       const promoSection = link.closest(".card-promo");
+      document.getElementById("_title_promos_view_").textContent = "Otras Promociones";
       
       if (promoSection) {
         promoSection.style.display = "none";
-        document.getElementById("_title_promos_view_").textContent = "Otras Promociones";
       }
     }
   });
