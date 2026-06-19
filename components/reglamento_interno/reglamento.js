aa4726f7b2e5f6d1fd4f0996ca7f031d3ed25423
juanfelipe <maflay26@gmail.com>
2026-06-19 10:50:07 -0500

    cambios reglamento,

--- components/reglamento_interno/reglamento.js
+++ components/reglamento_interno/reglamento.js
@@ -1,32 +1,52 @@
-// // 1. Ruta de tu archivo PDF
-// const rutaPDF = "/document/RIT_AHC_PUBLICADO_EN_LA_PAGINA.pdf";
-
-// // 2. Parámetros para ocultar la barra de herramientas, menú de navegación y zoom lateral
-// const parametrosPDF =
-//   "#toolbar=0&navpanes=0&scrollbar=1&statusbar=0&messages=0&view=FitH";
-
-// // 3. Seleccionar el contenedor
-// const contenedor = document.querySelector(
-//   "._content_main_reglamento_interno_ala_",
-// );
-
-// // 4. Insertar el iframe con los parámetros aplicados
-// contenedor.innerHTML = `<iframe src="${rutaPDF}${parametrosPDF}" width="100%" height="100%" style="border: none;"></iframe>`;
-
 (() => {
-    const _contenedor_reglamento_ = document.getElementById("_contenedor_reglamento_");
-    let longitud_reglamento = 80;
+  const _check_ala_ = document.getElementById("_check_ala_");
+  const _check_fox_ = document.getElementById("_check_fox_");
+  const _contenedor_reglamento_ = document.getElementById(
+    "_contenedor_reglamento_",
+  );
+  const _contenedor_reglamento_fox_ = document.getElementById(
+    "_contenedor_reglamento_fox_",
+  );
 
-    for (let i = 1; i <= longitud_reglamento; i++) { // Empezamos en 1 para las páginas
-        // Convierte el número a texto y asegura 2 dígitos (ej: 1 -> "01", 10 -> "10")
-        let numeroPagina = String(i).padStart(2, "0"); 
-        
-        let img = document.createElement("img");
-        
-        // Corregido: Se eliminó el 'src=' repetido dentro del string
-        img.src = `/resources/Recursos_reglamento/RIT_AHC_PUBLICADO_EN_LA_PAGINA_compressed_page-00${numeroPagina}.jpg`;
-        
-        // Inserta la imagen en tu contenedor HTML
-        _contenedor_reglamento_.appendChild(img);
+  _check_ala_.addEventListener("change", () => {
+    if (_check_ala_.checked == true) {
+      _check_fox_.checked = false;
+      _contenedor_reglamento_.style.display = "flex";
+      _contenedor_reglamento_fox_.style.display = "none";
+    } else {
+      _check_fox_.checked = false;
+      _contenedor_reglamento_.style.display = "none";
+      _contenedor_reglamento_fox_.style.display = "none";
     }
+  });
+
+  _check_fox_.addEventListener("change", () => {
+    if (_check_fox_.checked == true) {
+      _check_ala_.checked = false;
+      _contenedor_reglamento_.style.display = "none";
+      _contenedor_reglamento_fox_.style.display = "flex";
+    } else {
+      _check_ala_.checked = false;
+      _contenedor_reglamento_.style.display = "none";
+      _contenedor_reglamento_fox_.style.display = "none";
+    }
+  });
+
+  let longitud_reglamento = 80;
+
+  for (let i = 1; i <= longitud_reglamento; i++) {
+    let numeroPagina = String(i).padStart(2, "0");
+    let img = document.createElement("img");
+    img.src = `/resources/Recursos_reglamento/RIT_AHC_PUBLICADO_EN_LA_PAGINA_compressed_page-00${numeroPagina}.jpg`;
+    _contenedor_reglamento_.appendChild(img);
+  }
+
+  let longitud_reglamento_fox = 79;
+
+  for (let i = 1; i <= longitud_reglamento_fox; i++) {
+    let numeroPagina_fox = String(i).padStart(2, "0");
+    let img = document.createElement("img");
+    img.src = `/resources/Recursos_reglamento/reglamento_fox/RIT FOX PUBLICADO EN LA PAGINA_page-00${numeroPagina_fox}.jpg`;
+    _contenedor_reglamento_fox_.appendChild(img);
+  }
 })();
